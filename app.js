3a55372bf2b1a0bf168c9f13229eb2d56a0358ca
0-AB <122486093+0-AB@users.noreply.github.com>
2023-01-18 07:03:35 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const dotenv = require('dotenv').config();
 // replace the value below with the Telegram token you receive from @BotFather
-const token = process.env.API;
+const token = process.env.APIT;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
